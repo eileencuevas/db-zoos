54ed4e5fcc18d0aeffef8a896b04c837194a0a49
steelworx <eileencuevas62@gmail.com>
2019-01-15 16:31:27 -0500

    added DELETE endpoint in index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ server.get('/api/zoos/:id', (req, res) => {
         res.status(404).json({ message: 'No zoo found with that id.' })
       }
     })
-    .catch(error => {
+    .catch(() => {
       res.status(500).json({
         "error": "Could not retrieve any Zoos. Please try again."
       })
@@ -34,7 +34,7 @@ server.get('/api/zoos/', (req, res) => {
     .then(zoos => {
       res.status(200).json(zoos);
     })
-    .catch(error => {
+    .catch(() => {
       res.status(500).json({
         "error": "Could not retrieve any Zoos. Please try again."
       })
@@ -47,9 +47,27 @@ server.post('/api/zoos', (req, res) => {
     .then(zooIds => {
       res.status(201).json(zooIds);
     })
-    .catch(error => {
+    .catch(() => {
       res.status(500).json({
         "error": "Could not add Zoo. Make sure to use a unique name and try again."
+      });
+    });
+})
+
+server.delete('/api/zoos/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      if (count) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ message: 'No zoo found with that id.' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        "error": "Could not delete any Zoos. Please try again."
       });
     });
 })
