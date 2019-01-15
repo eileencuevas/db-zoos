194c9aafa22b6c42bc3e863e64c9e6412ceb66e3
steelworx <eileencuevas62@gmail.com>
2019-01-15 17:06:29 -0500

    added DELETE endpoint for bears

--- api/bears/bearsRouter.js
+++ api/bears/bearsRouter.js
@@ -47,4 +47,22 @@ router.post('/', (req, res) => {
         });
 })
 
+router.delete('/:id', (req, res) => {
+    db('bears')
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            if (count) {
+                res.status(200).json(count);
+            } else {
+                res.status(404).json({ message: 'No bear found with that id.' });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({
+                "error": "Could not add Bear. Make sure to use a unique name and try again."
+            });
+        });
+})
+
 module.exports = router;
