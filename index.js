1714888e944960ec299ba36d8121de6f1cf16427
steelworx <eileencuevas62@gmail.com>
2019-01-15 16:22:00 -0500

    added knex configeration and added POST endpoint in index.js

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile');
 
 const server = express();
+const db = knex(knexConfig.development);
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.post('/api/zoos', (req, res) => {
+  db('zoos')
+    .insert(req.body)
+    .then(zooIds => {
+      res.status(201).json(zooIds);
+    })
+    .catch(error => {
+      res.status(500).json({
+        "error": "Could not add Zoo. Make sure to use a unique name and try again."
+      });
+    });
+})
 
 const port = 3300;
 server.listen(port, function() {
