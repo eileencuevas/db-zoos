8da955b73ca2a5b7e844c85def6bcfb0578ee4a7
steelworx <eileencuevas62@gmail.com>
2019-01-15 16:36:10 -0500

    added PUT endpoint to index.js

--- index.js
+++ index.js
@@ -72,6 +72,26 @@ server.delete('/api/zoos/:id', (req, res) => {
     });
 })
 
+server.put('/api/zoos/:id', (req, res) => {
+  const newName = req.body;
+
+  db('zoos')
+    .where({ id: req.params.id })
+    .update(newName)
+    .then(count => {
+      if (count) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ message: 'No zoo found with that id.' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        "error": "Could not update any Zoos. Please try again."
+      });
+    });
+})
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
